1271c0859a4163caf2e5ed2f471c9f7ff6e85d0d
Andrés Clúa <andresclua@gmail.com>
2025-01-31 15:34:36 +0100

    update

--- dist/infiniteMarquee.es.js
+++ dist/infiniteMarquee.es.js
@@ -1,112 +1,98 @@
-import $, { gsap as i } from "gsap";
-function J(n, s) {
-  let h;
-  return n = i.utils.toArray(n), s = s || {}, i.context(() => {
-    let x = s.onChange, P = 0, e = i.timeline({ repeat: s.repeat, onUpdate: x && function() {
-      let t = e.closestIndex();
-      P !== t && (P = t, x(n[t], t));
-    }, paused: s.paused, defaults: { ease: "none" }, onReverseComplete: () => e.totalTime(e.rawTime() + e.duration() * 100) }), u = n.length, b = n[0].offsetLeft, d = [], g = [], y = [], w = [], l = 0, T = !1, f = s.center, I = (s.speed || 1) * 100, v = s.snap === !1 ? (t) => t : i.utils.snap(s.snap || 1), W = 0, X = f === !0 ? n[0].parentNode : i.utils.toArray(f)[0] || n[0].parentNode, m, j = () => n[u - 1].offsetLeft + w[u - 1] / 100 * g[u - 1] - b + y[0] + n[u - 1].offsetWidth * i.getProperty(n[u - 1], "scaleX") + (parseFloat(s.paddingRight) || 0), A = () => {
-      let t = X.getBoundingClientRect(), r;
-      n.forEach((o, a) => {
-        g[a] = parseFloat(i.getProperty(o, "width", "px")), w[a] = v(parseFloat(i.getProperty(o, "x", "px")) / g[a] * 100 + i.getProperty(o, "xPercent")), r = o.getBoundingClientRect(), y[a] = r.left - (a ? t.right : t.left), t = r;
-      }), i.set(n, {
-        // convert "x" to "xPercent" to make things responsive, and populate the widths/xPercents Arrays to make lookups faster.
-        xPercent: (o) => w[o]
-      }), m = j();
-    }, C, z = () => {
-      W = f ? e.duration() * (X.offsetWidth / 2) / m : 0, f && d.forEach((t, r) => {
-        d[r] = C(e.labels["label" + r] + e.duration() * g[r] / 2 / m - W);
-      });
-    }, D = (t, r, o) => {
-      let a = t.length, p = 1e10, R = 0, c;
-      for (; a--; )
-        c = Math.abs(t[a] - r), c > o / 2 && (c = o - c), c < p && (p = c, R = a);
-      return R;
-    }, O = () => {
-      let t, r, o, a, p;
-      for (e.clear(), t = 0; t < u; t++)
-        r = n[t], o = w[t] / 100 * g[t], a = r.offsetLeft + o - b + y[0], p = a + g[t] * i.getProperty(r, "scaleX"), e.to(r, { xPercent: v((o - p) / g[t] * 100), duration: p / I }, 0).fromTo(r, { xPercent: v((o - p + m) / g[t] * 100) }, { xPercent: w[t], duration: (o - p + m - o) / I, immediateRender: !1 }, p / I).add("label" + t, a / I), d[t] = a / I;
-      C = i.utils.wrap(0, e.duration());
-    }, k = (t) => {
-      let r = e.progress();
-      e.progress(0, !0), A(), t && O(), z(), t && e.draggable ? e.time(d[l], !0) : e.progress(r, !0);
-    }, F = () => k(!0), B;
-    i.set(n, { x: 0 }), A(), O(), z(), window.addEventListener("resize", F);
-    function M(t, r) {
-      r = r || {}, Math.abs(t - l) > u / 2 && (t += t > l ? -u : u);
-      let o = i.utils.wrap(0, u, t), a = d[o];
-      return a > e.time() != t > l && t !== l && (a += e.duration() * (t > l ? 1 : -1)), (a < 0 || a > e.duration()) && (r.modifiers = { time: C }), l = o, r.overwrite = !0, i.killTweensOf(B), r.duration === 0 ? e.time(C(a)) : e.tweenTo(a, r);
+import g, { gsap as x } from "gsap";
+function $(n, s) {
+  n = x.utils.toArray(n), s = s || {};
+  let i = x.timeline({ repeat: s.repeat, paused: s.paused, defaults: { ease: "none" }, onReverseComplete: () => i.totalTime(i.rawTime() + i.duration() * 100) }), y = n.length, M = n[0].offsetLeft, e = [], c = [], b = [], l = 0, P = (s.speed || 1) * 100, C = s.snap === !1 ? (p) => p : x.utils.snap(s.snap || 1), I, d, v, R, T, m;
+  for (x.set(n, {
+    // convert "x" to "xPercent" to make things responsive, and populate the widths/xPercents Arrays to make lookups faster.
+    xPercent: (p, f) => {
+      let A = c[p] = parseFloat(x.getProperty(f, "width", "px"));
+      return b[p] = C(parseFloat(x.getProperty(f, "x", "px")) / A * 100 + x.getProperty(f, "xPercent")), b[p];
     }
-    if (e.toIndex = (t, r) => M(t, r), e.closestIndex = (t) => {
-      let r = D(d, e.time(), e.duration());
-      return t && (l = r, T = !1), r;
-    }, e.current = () => T ? e.closestIndex(!0) : l, e.next = (t) => M(e.current() + 1, t), e.previous = (t) => M(e.current() - 1, t), e.times = d, e.progress(1, !0).progress(0, !0), s.reversed && (e.vars.onReverseComplete(), e.reverse()), s.draggable && typeof Draggable == "function") {
-      B = document.createElement("div");
-      let t = i.utils.wrap(0, 1), r, o, a, p, R, c, N = () => e.progress(t(o + (a.startX - a.x) * r)), S = () => e.closestIndex(!0);
-      typeof InertiaPlugin > "u" && console.warn("InertiaPlugin required for momentum-based scrolling and snapping. https://greensock.com/club"), a = Draggable.create(B, {
+  }), x.set(n, { x: 0 }), I = n[y - 1].offsetLeft + b[y - 1] / 100 * c[y - 1] - M + n[y - 1].offsetWidth * x.getProperty(n[y - 1], "scaleX") + (parseFloat(s.paddingRight) || 0), m = 0; m < y; m++)
+    T = n[m], d = b[m] / 100 * c[m], v = T.offsetLeft + d - M, R = v + c[m] * x.getProperty(T, "scaleX"), i.to(T, { xPercent: C((d - R) / c[m] * 100), duration: R / P }, 0).fromTo(T, { xPercent: C((d - R + I) / c[m] * 100) }, { xPercent: b[m], duration: (d - R + I - d) / P, immediateRender: !1 }, R / P).add("label" + m, v / P), e[m] = v / P;
+  function L(p, f) {
+    f = f || {}, Math.abs(p - l) > y / 2 && (p += p > l ? -y : y);
+    let A = x.utils.wrap(0, y, p), E = e[A];
+    return E > i.time() != p > l && (f.modifiers = { time: x.utils.wrap(0, i.duration()) }, E += i.duration() * (p > l ? 1 : -1)), l = A, f.overwrite = !0, i.tweenTo(E, f);
+  }
+  return i.next = (p) => L(l + 1, p), i.previous = (p) => L(l - 1, p), i.current = () => l, i.toIndex = (p, f) => L(p, f), i.times = e, i.progress(1, !0).progress(0, !0), s.reversed && (i.vars.onReverseComplete(), i.reverse()), i;
+}
+function G(n, s) {
+  let i;
+  return n = g.utils.toArray(n), s = s || {}, g.context(() => {
+    let y = s.onChange, M = 0, e = g.timeline({ repeat: s.repeat, onUpdate: y && function() {
+      let t = e.closestIndex();
+      M !== t && (M = t, y(n[t], t));
+    }, paused: s.paused, defaults: { ease: "none" }, onReverseComplete: () => e.totalTime(e.rawTime() + e.duration() * 100) }), c = n.length, b = n[0].offsetTop, l = [], P = [], C = [], I = [], d = 0, v = s.center, R = (t) => {
+      let r = {}, o;
+      for (o in t)
+        r[o] = t[o];
+      return r;
+    }, T = (s.speed || 1) * 100, m = s.snap === !1 ? (t) => t : g.utils.snap(s.snap || 1), L = 0, p = v === !0 ? n[0].parentNode : g.utils.toArray(v)[0] || n[0].parentNode, f, A = () => n[c - 1].offsetTop + I[c - 1] / 100 * P[c - 1] - b + C[0] + n[c - 1].offsetHeight * g.getProperty(n[c - 1], "scaleY") + (parseFloat(s.paddingBottom) || 0), E = () => {
+      let t = p.getBoundingClientRect(), r;
+      b = n[0].offsetTop, n.forEach((o, a) => {
+        P[a] = parseFloat(g.getProperty(o, "height", "px")), I[a] = m(parseFloat(g.getProperty(o, "y", "px")) / P[a] * 100 + g.getProperty(o, "yPercent")), r = o.getBoundingClientRect(), C[a] = r.top - (a ? t.bottom : t.top), t = r;
+      }), g.set(n, {
+        // convert "x" to "xPercent" to make things responsive, and populate the widths/xPercents Arrays to make lookups faster.
+        yPercent: (o) => I[o]
+      }), f = A();
+    }, F, D = () => {
+      L = v ? e.duration() * (p.offsetWidth / 2) / f : 0, v && l.forEach((t, r) => {
+        l[r] = F(e.labels["label" + r] + e.duration() * P[r] / 2 / f - L);
+      });
+    }, H = (t, r, o) => {
+      let a = t.length, u = 1e10, h = 0, w;
+      for (; a--; )
+        w = Math.abs(t[a] - r), w > o / 2 && (w = o - w), w < u && (u = w, h = a);
+      return h;
+    }, O = () => {
+      let t, r, o, a, u;
+      for (e.clear(), t = 0; t < c; t++)
+        r = n[t], o = I[t] / 100 * P[t], a = r.offsetTop + o - b + C[0], u = a + P[t] * g.getProperty(r, "scaleY"), e.to(r, { yPercent: m((o - u) / P[t] * 100), duration: u / T }, 0).fromTo(r, { yPercent: m((o - u + f) / P[t] * 100) }, { yPercent: I[t], duration: (o - u + f - o) / T, immediateRender: !1 }, u / T).add("label" + t, a / T), l[t] = a / T;
+      F = g.utils.wrap(0, e.duration());
+    }, S = () => {
+      let { enterAnimation: t, leaveAnimation: r } = s, o = e.duration() / n.length;
+      n.forEach((a, u) => {
+        let h = t && t(a, o, u), w = h && e.duration() - F(l[u] - Math.min(o, h.duration())) < o - 0.05;
+        h && e.add(h, w ? 0 : F(l[u] - h.duration())), h = r && r(a, o, u), w = l[u] === e.duration(), h && h.duration() > o && h.duration(o), h && e.add(h, w ? 0 : l[u]);
+      });
+    }, W = (t) => {
+      let r = e.progress();
+      e.progress(0, !0), E(), t && O(), D(), S(), t && e.draggable ? e.time(l[d], !0) : e.progress(r, !0);
+    }, X = () => W(!0), Y;
+    g.set(n, { y: 0 }), E(), O(), D(), S(), window.addEventListener("resize", X);
+    function B(t, r) {
+      r = R(r), Math.abs(t - d) > c / 2 && (t += t > d ? -c : c);
+      let o = g.utils.wrap(0, c, t), a = l[o];
+      return a > e.time() != t > d && (a += e.duration() * (t > d ? 1 : -1)), r.revolutions && (a += e.duration() * Math.round(r.revolutions), delete r.revolutions), (a < 0 || a > e.duration()) && (r.modifiers = { time: F }), d = o, r.overwrite = !0, g.killTweensOf(Y), e.tweenTo(a, r);
+    }
+    if (e.elements = n, e.next = (t) => B(d + 1, t), e.previous = (t) => B(d - 1, t), e.current = () => d, e.toIndex = (t, r) => B(t, r), e.closestIndex = (t) => {
+      let r = H(l, e.time(), e.duration());
+      return t && (d = r), r;
+    }, e.times = l, e.progress(1, !0).progress(0, !0), s.reversed && (e.vars.onReverseComplete(), e.reverse()), s.draggable && typeof Draggable == "function") {
+      Y = document.createElement("div");
+      let t = g.utils.wrap(0, 1), r, o, a, u = () => e.progress(t(o + (a.startY - a.y) * r)), h = () => e.closestIndex(!0);
+      typeof InertiaPlugin > "u" && console.warn("InertiaPlugin required for momentum-based scrolling and snapping. https://gsap.com/pricing"), a = Draggable.create(Y, {
         trigger: n[0].parentNode,
-        type: "x",
+        type: "y",
         onPressInit() {
-          let E = this.x;
-          i.killTweensOf(e), c = !e.paused(), e.pause(), o = e.progress(), k(), r = 1 / m, R = o / -r - E, i.set(B, { x: o / -r });
+          g.killTweensOf(e), o = e.progress(), W(), r = 1 / f, g.set(Y, { y: o / -r });
         },
-        onDrag: N,
-        onThrowUpdate: N,
-        overshootTolerance: 0,
+        onDrag: u,
+        onThrowUpdate: u,
         inertia: !0,
-        snap(E) {
-          if (Math.abs(o / -r - this.x) < 10)
-            return p + R;
-          let U = -(E * r) * e.duration(), q = C(U), G = d[D(d, q, e.duration())], L = G - q;
-          return Math.abs(L) > e.duration() / 2 && (L += L < 0 ? e.duration() : -e.duration()), p = (U + L) / e.duration() / -r, p;
+        snap: (w) => {
+          let N = -(w * r) * e.duration(), k = F(N), U = l[H(l, k, e.duration())], z = U - k;
+          return Math.abs(z) > e.duration() / 2 && (z += z < 0 ? e.duration() : -e.duration()), (N + z) / e.duration() / -r;
         },
-        onRelease() {
-          S(), a.isThrowing && (T = !0);
-        },
-        onThrowComplete: () => {
-          S(), c && e.play();
-        }
+        onRelease: h,
+        onThrowComplete: h
       })[0], e.draggable = a;
     }
-    return e.closestIndex(!0), P = l, x && x(n[l], l), h = e, () => window.removeEventListener("resize", F);
-  }), h;
-}
-function K(n, s) {
-  if (n = $.utils.toArray(n), !Array.isArray(n) || n.length === 0)
-    return null;
-  s = s || {};
-  let h = n[0].getBoundingClientRect(), x = n[n.length - 1].getBoundingClientRect();
-  if (!h || !x)
-    return null;
-  let P = h.top - h.height - Math.abs(n[1].getBoundingClientRect().top - h.bottom), e = x.top, u = e - P, b = s.speed * 100 || 1, d = Math.abs(u / b), g = $.timeline({ repeat: s.repeat || -1, paused: s.paused || !1 }), y = b < 0 ? "-=" : "+=", w = b < 0 ? "+=" : "-=";
-  return n.forEach((l) => {
-    if (l) {
-      let T = l.getBoundingClientRect(), f = Math.abs((e - T.top) / u);
-      b < 0 && (f = 1 - f), g.to(
-        l,
-        {
-          y: y + u * f,
-          duration: d * f,
-          ease: "none"
-        },
-        0
-      ), g.fromTo(
-        l,
-        {
-          y: w + u
-        },
-        {
-          y: y + (1 - f) * u,
-          ease: "none",
-          duration: (1 - f) * d,
-          immediateRender: !1
-        },
-        d * f
-      );
-    }
-  }), g;
+    return e.closestIndex(!0), y && y(n[d], d), i = e, () => window.removeEventListener("resize", X);
+  }), i;
 }
 export {
-  J as horizontalLoop,
-  K as verticalLoop
+  $ as horizontalLoop,
+  G as verticalLoop
 };
